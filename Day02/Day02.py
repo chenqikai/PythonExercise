5a6d1b4074de3e2328988278f7a4a50e3d791666
chenqikai <1312664470@qq.comg>
2019-06-22 19:19:39 +0800

    提交作业

--- Day02/Day02.py
+++ Day02/Day02.py
@@ -1,4 +1,5 @@
 import random
+import keyword
 
 """
 列表 ： 类似数组，元素可以是任意数据类型
@@ -55,3 +56,49 @@ while True:
         print('列表以清空')
         print(List2)
         break
+
+print(keyword.kwlist)
+str = 'Elichen'
+
+print(str)  # 输出字符串
+print(str[0:-1])  # 输出第一个到倒数第二个的所有字符
+print(str[0])  # 输出字符串第一个字符
+print(str[2:5])  # 输出从第三个开始到第五个的字符
+print(str[2:])  # 输出从第三个开始的后的所有字符
+print(str * 2)  # 输出字符串两次
+print(str + "TEST")  # 连接字符串
+
+list = ['abcd', 786, 2.23, 'elichen', 70.2]
+tinylist = [123, 'elichen']
+
+print(list)  # 输出完整列表
+print(list[0])  # 输出列表第一个元素
+print(list[1:3])  # 从第二个开始输出到第三个元素
+print(list[2:])  # 输出从第三个元素开始的所有元素
+print(tinylist * 2)  # 输出两次列表
+print(list + tinylist)  # 连接列表
+
+list1 = ['e', 'l', 'i', 'c', 'h', 'e', 'n']
+print(list1[1:4:2])
+
+tuple = ('abcd', 786, 2.23, 'elichen', 70.2)
+tinytuple = (123, 'elichen')
+
+print(tuple)  # 输出完整元组
+print(tuple[0])  # 输出元组的第一个元素
+print(tuple[1:3])  # 输出从第二个元素开始到第三个元素
+print(tuple[2:])  # 输出从第三个元素开始的所有元素
+print(tinytuple * 2)  # 输出两次元组
+print(tuple + tinytuple)  # 连接元组
+
+dict = {}
+dict['one'] = "1"
+dict[2] = "2"
+
+tinydict = {'name': 'elichen', 'code': 1, 'site': 'www.elichen.club'}
+
+print(dict['one'])  # 输出键为 'one' 的值
+print(dict[2])  # 输出键为 2 的值
+print(tinydict)  # 输出完整的字典
+print(tinydict.keys())  # 输出所有键
+print(tinydict.values())  # 输出所有值
